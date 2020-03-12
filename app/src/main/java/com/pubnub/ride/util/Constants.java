1de5b558ed9b00dcc6ea9a5681e2f2e6e4d3391a
ben supanga <ibmbendev@gmail.com>
2020-03-12 15:06:35 +0800

    added google key api

--- app/src/main/java/com/pubnub/ride/util/Constants.java
+++ app/src/main/java/com/pubnub/ride/util/Constants.java
@@ -1,8 +1,8 @@
 package com.pubnub.ride.util;
 
 public class Constants {
-    public final static String PUBNUB_PUBLISH_KEY = "YOUR_PUBLISH_KEY";
-    public final static String PUBNUB_SUBSCRIBE_KEY = "YOUR_SUBSCRIBE_KEY";
-    public final static String GOOGLE_API_KEY = "YOUR_GOOGLE_API_KEY";
+    public final static String PUBNUB_PUBLISH_KEY = "pub-c-aed4c637-df7d-44e4-a65e-cbb61fbc18a9";
+    public final static String PUBNUB_SUBSCRIBE_KEY = "sub-c-8e36f95a-cd4c-11e9-a7ce-f271ece57037";
+    public final static String GOOGLE_API_KEY = "AIzaSyBqZABqHw0DJ9oVg_LPch1SjOoXMXQmLek";
     public final static String PUBNUB_CHANNEL_NAME = "car_location";
 }
